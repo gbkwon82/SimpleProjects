20186f7e646293c1b30207a474c029b36b9189fb
gbkwon82 <gb918079@naver.com>
2017-03-23 23:17:09 +0900

    Initialize version

---
+++ xmlTest.py
@@ -0,0 +1,32 @@
+from xml.etree.ElementTree import ElementTree, Element, SubElement, dump
+
+note = Element("note")
+note.attrib["date"] = "20120104"
+
+to = Element("to")
+to.text = "Toove"
+note.append(to)
+
+SubElement(note, "from").text = "Jani"
+SubElement(note, "heading").text = "Reminder"
+SubElement(note, "body").text = "Don't forget me"
+
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+indent(note)
+dump(note)
+
+ElementTree(note).write("note.xml")
